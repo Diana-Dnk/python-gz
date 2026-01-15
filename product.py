5aa26504428506df2e206c7df9679c0e9ea3b5ca
Diana-Dnk <calorekz@gmail.com>
2026-01-15 12:36:11 +0500

    product

---
+++ product.py
@@ -0,0 +1,6 @@
+# # фунция поиска товара
+# def find_product(name):
+#     for product in products:
+#         if product["name"].lower()==name.lower():
+#             return product
+#     return None
